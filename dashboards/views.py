d3fe975d71795b957b9ad9b634eaa7da70375887
Moshiur Rahman <moshiur_cse@hotmail.com>
2023-04-05 13:24:44 +0600

    Implement Leaflet Map

--- dashboards/views.py
+++ dashboards/views.py
@@ -7,6 +7,11 @@ from _keenthemes.libs.theme import KTTheme
 from pprint import pprint
 from django.shortcuts import redirect, render
 
+from django.shortcuts import render
+from django.contrib.gis.gdal import DataSource
+from django.contrib.gis.geos import Point
+#from django.contrib.gis.maps.google import GoogleMap
+
 """
 This file is a view controller for multiple pages as a module.
 Here you can override the page view layout.
@@ -44,4 +49,26 @@ class DashboardsView(TemplateView):
         return context
     
     def map(request):
+        data_source = DataSource('/path/to/geotiff.tiff')
+
+    # Get the first layer from the file
+        layer = data_source[0]
+
+        # Create a GeoJSON object from the layer
+        geojson = layer.geojson
+
+        # Get the bounding box of the layer
+        bbox = layer.extent
+
+        # Create a Point object from the center of the bounding box
+        center = Point((bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2)
+
+        # Create a GoogleMap object with the center and zoom level
+        map = GoogleMap(center=center, zoom=10)
+
+        # Add the GeoJSON data to the map
+        map.add_layer(geojson)
+
+        # Render the map using a template
+        #return render(request, 'map.html', {'map': map})
         return render(request, 'map.html')
